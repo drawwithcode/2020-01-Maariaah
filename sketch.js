3646b76fb5f6008ed0b4d9f2840c50effc83639d
Marie <42433966+Maariaah@users.noreply.github.com>
2020-09-29 23:36:32 +0200

    First assignment

--- sketch.js
+++ sketch.js
@@ -1,12 +1,22 @@
-function preload(){
+function preload() {
   // put preload code here
 }
-
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  // put setup code here
+  createCanvas(600, 600);
+  //stroke(0.4);
+  noFill();
+  background(0,0,0);
 }
 
 function draw() {
-  // put drawing code here
+  var t = frameCount*2;
+  stroke(lerpColor(color(46, 255, 241, 20), color(250, 110, 22, 20), frameCount / 520));
+  translate(width / 2, height / 2);
+  rotate(t / 100);
+  rect(t / 140, t / 60, atan(t / 60) * 200, cos(t / 60) * 200);
+  frameRate(900);
+  if (frameCount >= 990) {
+    noLoop();
+  }
 }
+
