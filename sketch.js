c5550167a565535b52ba35f9aaeb985d9df93939
Marie <42433966+Maariaah@users.noreply.github.com>
2020-10-14 10:51:42 +0200

    Fix octal numeric literals

--- sketch.js
+++ sketch.js
@@ -1,21 +1,72 @@
-function preload() {
-  // put preload code here
-}
-function setup() {
-  createCanvas(600, 600);
-  noFill();
-  background(0,0,0);
-}
+"use strict";
 
-function draw() {
-  var t = frameCount*2;
-  stroke(lerpColor(color(46, 255, 241, 20), color(250, 110, 22, 20), frameCount / 520));
-  translate(width / 2, height / 2);
-  rotate(t / 100);
-  rect(t / 140, t / 60, atan(t / 60) * 200, cos(t / 60) * 200);
-  frameRate(900);
-  if (frameCount >= 990) {
-    noLoop();
+let eyesCollection = [];
+
+function setup() {
+  let colors = ["orange", "red", "blue"];
+
+  createCanvas(900, 900);
+  noStroke();
+  for (let i = 0; i < 10; i++) {
+    for (let c = 0; c < colors.length; c++) {
+      addEye(
+        Math.random() * 900,
+        Math.random() * 900,
+        Math.random() * 70,
+        colors[c]
+      );
+    }
   }
 }
 
+function draw() {
+  background('#0024');
+
+  for (let i = 0; i < eyesCollection.length; i++) {
+    eyesCollection[i].display();
+    eyesCollection[i].update(mouseX, mouseY);
+  }
+}
+
+function addEye(a, b, c, d) {
+  newEye = new Eye(a, b, c, d);
+  eyesCollection.push(newEye);
+}
+
+class Eye {
+  constructor(tx, ty, ts, tc) {
+    this.x = tx;
+    this.y = ty;
+    this.size = ts;
+    this.angle = 30;
+    this.color = tc;
+  }
+  update(mx, my) {
+    this.angle = atan2(my - this.y, mx - this.x);
+  }
+  display() {
+    push();
+
+    translate(this.x, this.y);
+    fill(255, 255, 255, 127);
+    beginShape();
+    vertex(-80, 0);
+    bezierVertex(-30, -50, 30, -50, 80, 0);
+    bezierVertex(30, 50, -30, 50, -80, 0);
+    endShape();
+
+    if (random(20) < 2) {
+      frameRate(1);
+      stroke(255);
+      line(-80, 00, -30 + 110, 0);
+    } else {
+      ellipse(0, 0, this.size + 10, this.size + 10);
+      rotate(this.angle);
+      fill(this.color);
+      ellipse(0, 0, this.size, this.size);
+      fill(0);
+      ellipse(this.size / 4, 0, this.size / 2, this.size / 1.7);
+    }
+    pop();
+  }
+}
